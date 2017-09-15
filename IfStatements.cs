f63865d34537c9f56b72bd4389cd8fd4516e3f4b
Unknown <citizenkaneVII@gmail.com>
2017-09-15 12:36:31 -0600

    Begining script

---
+++ IfStatements.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ */
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// This is indented once because its a method within the public class
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
