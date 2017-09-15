9a92784eccbb13844cac81a42f936995ca4ff2a2
Unknown <citizenkaneVII@gmail.com>
2017-09-15 12:49:09 -0600

    Intro

--- IfStatements.cs
+++ IfStatements.cs
@@ -2,18 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/*
+/*If statements inform our program what to do based on what conditions
  */
 
-public class NewBehaviourScript : MonoBehaviour {
+public class ifStatement : MonoBehaviour {
 
-	// This is indented once because its a method within the public class
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
+	public Text input;
+
+	public bool canPlayGame = false;
+	public string password = "G@am3Pl@y";
+
 	void Update () {
-		
+		if(input.text == password) {
+			print("You know the password.");
+			canPlayGame = true;
+		} 
+        else {
+			print("The password is incorrect.");
+		}
+
+		if(canPlayGame) {
+			print("Playing Game");
+		}	
+        else {
+			print("Can't Play Yet, Enter a Correct Password.");
+		}
 	}
 }
