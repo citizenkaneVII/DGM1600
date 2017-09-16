c65c693330e30cad935eba63c6761523613a70c6
Unknown <citizenkaneVII@gmail.com>
2017-09-16 13:57:56 -0600

    Further Examples

--- IfStatements.cs
+++ IfStatements.cs
@@ -1,4 +1,4 @@
-/*When all else fails, perform the function of the else clause. */
+/*Further examples.*/
 
 
 using System;
@@ -7,34 +7,28 @@ class Program
 {
     static void Main()
     {
-        // Call method with embedded if-statement three times.
-        int result1 = Test(0);
-        int result2 = Test(50);
-        int result3 = Test(-1);
-
-        // Print results.
-        Console.WriteLine(result1);
-        Console.WriteLine(result2);
-        Console.WriteLine(result3);
+        Console.WriteLine(A(5));
+        Console.WriteLine(B(4));
     }
 
-    static int Test(int value)
+    static bool A(int y)
     {
-        if (value == 0)
+        if (y >= 5)
         {
-            return -1;
-        }
-        else if (value <= 10)
-        {
-            return 0;
-        }
-        else if (value <= 100)
-        {
-            return 1;
+            return true;
         }
         else
         {
-            return 2;
+            return false;
         }
+    }
+
+    static bool B(int y)
+    {
+        if (y >= 5)
+        {
+            return true;
+        }
+        return false;
     }
 }
