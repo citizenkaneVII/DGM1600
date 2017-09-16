e19dfac17c99765bf010417582bd0dadcd4a1fcf
Unknown <citizenkaneVII@gmail.com>
2017-09-16 13:56:11 -0600

    Else 5

--- IfStatements.cs
+++ IfStatements.cs
@@ -1,43 +1,40 @@
-/*But if a is not equel to 10, 20 or 30, that means none of the conditions are true, and as a result, the last option, the else condtion will
-perform it's associated function, printing "None of the values is matching". */
+/*When all else fails, perform the function of the else clause. */
 
 
 using System;
-namespace DecisionMaking
+
+class Program
 {
-   class Program 
-   {
-      static void Main(string[] args)
-      {
-         /* local variable definition */
-         int a = 100;
-         
-         /* check the boolean condition */
-         if (a == 10)
-         {
-            /* if condition is true then print the following */
-            Console.WriteLine("Value of a is 10");
-         }
-         
-         else if (a == 20)
-         {
-            /* if else if condition is true */
-            Console.WriteLine("Value of a is 20");
-         }
-         
-         else if (a == 30)
-         {
-            /* if else if condition is true  */
-            Console.WriteLine("Value of a is 30");
-         }
-         
-         else
-         {
-            /* if none of the conditions is true */
-            Console.WriteLine("None of the values is matching");
-         }
-         Console.WriteLine("Exact value of a is: {0}", a);
-         Console.ReadLine();
-      }
-   }
+    static void Main()
+    {
+        // Call method with embedded if-statement three times.
+        int result1 = Test(0);
+        int result2 = Test(50);
+        int result3 = Test(-1);
+
+        // Print results.
+        Console.WriteLine(result1);
+        Console.WriteLine(result2);
+        Console.WriteLine(result3);
+    }
+
+    static int Test(int value)
+    {
+        if (value == 0)
+        {
+            return -1;
+        }
+        else if (value <= 10)
+        {
+            return 0;
+        }
+        else if (value <= 100)
+        {
+            return 1;
+        }
+        else
+        {
+            return 2;
+        }
+    }
 }
