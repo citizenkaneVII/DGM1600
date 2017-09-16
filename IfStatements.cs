1863f6be2a0d3dd35d6670a06315a1805fb1b675
Unknown <citizenkaneVII@gmail.com>
2017-09-16 14:01:08 -0600

    Coffee Example

--- IfStatements.cs
+++ IfStatements.cs
@@ -1,29 +1,43 @@
 /*Further examples.*/
 
+using UnityEngine;
+using System.Collections;
 
-using System;
-
-namespace CodeStudies
+public class IfStatements : MonoBehaviour
 {
-    class Program
+    float coffeeTemperature = 85.0f;
+    float hotLimitTemperature = 70.0f;
+    float coldLimitTemperature = 40.0f;
+    
+
+    void Update ()
     {
-        static void Main(string[] args)
+        if(Input.GetKeyDown(KeyCode.Space))
+            TemperatureTest();
+        
+        coffeeTemperature -= Time.deltaTime * 5f;
+    }
+    
+    
+    void TemperatureTest ()
+    {
+        // If the coffee's temperature is greater than the hottest drinking temperature...
+        if(coffeeTemperature > hotLimitTemperature)
         {
-            int linesOfCodeWritten = 75;
-
-            if (linesOfCodeWritten <= 25)
-            {
-                Console.WriteLine("Not too bad; less code can certainly be more.");
-            }
-            else
-            {
-                Console.WriteLine("You added more than 25 lines of code to the project.");
-
-                if (linesOfCodeWritten > 500)
-                {
-                    Console.WriteLine("Working hard on some kind of new feature?");
-                }
-            }
+            // ... do this.
+            print("Coffee is too hot.");
+        }
+        // If it isn't, but the coffee temperature is less than the coldest drinking temperature...
+        else if(coffeeTemperature < coldLimitTemperature)
+        {
+            // ... do this.
+            print("Coffee is too cold.");
+        }
+        // If it is neither of those then...
+        else
+        {
+            // ... do this.
+            print("Coffee is just right.");
         }
     }
 }
