7b4ae01180c79c2c9e64711c2bff13f81be4898c
Unknown <citizenkaneVII@gmail.com>
2017-09-16 19:35:30 -0600

    Oporators

--- Math Operators.cs
+++ Math Operators.cs
@@ -1,1 +1,10 @@
-/*Math opporators are used to perform caculations in a program. */
+/*Math opporators are used to perform caculations in a program.
+The opporators used are:
+
++ to add/find sum
+- to subtract/find difference
+* to multiply/find product
+/ to devide/find quotient
+% modulus/to find remainder
+
+ */
