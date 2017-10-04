9b6679c9f055c7ce7f1a1513f9c778d26d6be783
Unknown <citizenkaneVII@gmail.com>
2017-10-04 14:28:35 -0600

    Example

--- Syntax.cs
+++ Syntax.cs
@@ -6,3 +6,27 @@ With out this, programing because difficult to do. */
 /*First off are comments.  Comments are small descriptions of what the following block of code does.
 
 And example of this is: */
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RunGame : MonoBehaviour {
+
+	public Toggle toggle;
+    public float health = 100;
+
+    // Update is called once per frame
+    void Update () {
+		if (toggle.isOn)
+		{
+			print("Play Game");
+		} else {
+			print("Can't Play.");
+		}
+
+		health -= 0.01f;
+	}
+}
