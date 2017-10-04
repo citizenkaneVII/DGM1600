e7cbebbae4ebde679942dc0b2c7725f38b5f1911
Unknown <citizenkaneVII@gmail.com>
2017-10-04 14:24:09 -0600

    Intro

--- Syntax.cs
+++ Syntax.cs
@@ -1,1 +1,4 @@
-//Syntax Assignment
+//Syntax Assignment and conventions
+
+/*When coding, syntax is important because it helps everyone involved be able to read and understand the code.
+With out this, programing because difficult to do. */
