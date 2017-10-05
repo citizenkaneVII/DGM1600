ed8304b3406e36d0897d50ad60921ec855956a7d
Unknown <citizenkaneVII@gmail.com>
2017-10-05 00:04:58 -0600

    examples

--- Math Operators.cs
+++ Math Operators.cs
@@ -14,5 +14,17 @@ The opporators used are:
     B / A = 2
     B % A = 0
 
+There is also ++ and -- which are used in loops.  These will either increse or decrese and interger by 1.
+
+int a = 6;
+int b = 3;
+int result;
+ 
+result = a + b;         // result = 9
+result = a - b;         // result = 3
+result = a * b;         // result = 18
+result = a / b;         // result = 2
+result = a + b - 1;     // result = 8
+
 
  */
