6b6e5b49c0e7955989ff2f1c739949ea41392a90
Unknown <citizenkaneVII@gmail.com>
2017-10-06 00:25:39 -0600

    one more

---
+++ Math Operators.cs
@@ -0,0 +1,52 @@
+/*Math opporators are used to perform caculations in a program.
+The opporators used are:
+
++ to add/find sum
+- to subtract/find difference
+* to multiply/find product
+/ to devide/find quotient
+% modulus/to find remainder
+
+
+ 	A + B = 30
+    A - B = -10
+    A * B = 200
+    B / A = 2
+    B % A = 0
+
+    int a = 6;
+int b = 3;
+int result;
+ 
+result = a + b;         // result = 9
+result = a - b;         // result = 3
+result = a * b;         // result = 18
+result = a / b;         // result = 2
+result = a + b - 1;     // result = 8
+
+
+There is also ++ and -- which are used in loops.  These will either increse or decrese and interger by 1.
+
+
+++9 == 10
+
+for (i = 0; i < 100; i++){
+   for (j = i; j < 10; j++){
+      some_function(i, j);
+
+Other examples of aritmetic operators:
+
+int a = 7;
+int b = 3;
+int integerResult;
+float floatResult;
+ 
+integerResult = a / b;          // integerResult = 2  (truncated)
+floatResult = a / b;            // floatResult = 2.0  (truncated)
+floatResult = (float)a / b;     // floatResult = 2.33333325
+
+
+
+
+
+ */
