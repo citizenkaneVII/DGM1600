bd2a43d7c2b8cc5a0a33eeb7efd90beb2a335ea1
Unknown <citizenkaneVII@gmail.com>
2017-10-06 00:33:39 -0600

    Arrays 2

--- Arrays.cs
+++ Arrays.cs
@@ -1,5 +1,11 @@
 /*So you know how we have variables? */
 
-int x = 12
+int x = 12;
 
-/* */
+//Let's say we have a bunch of those.  They're all the same type, but there's alot of them.
+
+string grades = "5";
+string grades = "7";
+string grades = "3";
+
+//Instead of dealing with making a bunch of these, we can make an array.
