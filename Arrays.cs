7db0d38f4988b5a93f1e4b45854dac05d81287a5
Unknown <citizenkaneVII@gmail.com>
2017-10-06 12:14:04 -0600

    Array 9

--- Arrays.cs
+++ Arrays.cs
@@ -14,16 +14,16 @@ string[] grades = {"5", "7", "3"};
 
 //An array is a fixed number of elements or a particular type.  All of these can have different values.
 
-Color[] colors = {Color.red, Color.blue, Color.black, Color.yellow};
+public Color[] colors = {Color.red, Color.blue, Color.black, Color.yellow};
 
 //When declairing an array, it's important to define wether it's public or private.
 
 public int[] hight = {3, 4, 4, 7, 2};
 
 //Public will allow you to see the array in the inspector.
+//The following is an array that can be adjusted in the inspector.
 
-
-int[] myIntArray = new int [5];
+public int[] myIntArray = new int [5];
 void start (){
     myIntArray[0] = 63;
     myIntArray[1] = 72;
@@ -32,5 +32,18 @@ void start (){
     myIntArray[4] = 99;
 }
 
+/*Any items in the inspector can be adjusted, including how many elements are in the array and how many.
+To do that is not hard coding./* */
 
-string[] batPeeps = {"Alfred", "Robin", "Oracle", "Nightwing", "Catwoman", "Gordon",}
+private string[] batPeeps = {"Alfred", "Robin", "Oracle", "Nightwing", "Catwoman", "Comm. Gordon", "Lucius Fox"};
+
+//It is important to name our array.  For instance, the following will bring up an error.
+
+public string[] = {"Aang", "Katara", "Sokka", "Toph", "Zuko", "Suki"};
+
+//Instead, it should be like this:
+
+public string[] teamAvatar= {"Aang", "Katara", "Sokka", "Toph", "Zuko", "Suki"};
+
+
+
