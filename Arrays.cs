e871b0d0012c72bc15bf6517fe2c638e2e9f8cf5
Unknown <citizenkaneVII@gmail.com>
2017-10-06 01:06:25 -0600

    Arrays 5

--- Arrays.cs
+++ Arrays.cs
@@ -10,9 +10,16 @@ string grades = "3";
 
 //Instead of dealing with making a bunch of these, we can make an array.
 
-sting[] grades = {"5", "7", "3"};
+string[] grades = {"5", "7", "3"};
 
 //An array is a fixed number of elements or a particular type.  All of these can have different values.
 
-color [] colors = {Color.red, Color.blue, Color.black, Color.yellow};
+Color[] colors = {Color.red, Color.blue, Color.black, Color.yellow};
+
+
+int[] hight = {3, 4, 4, 7, 2};
+
+
+
+int[] myIntArray = new int [5];
 
