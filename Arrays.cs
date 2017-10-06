25ba1db7a98e897229812258af5a5c8cfbaf0823
Unknown <citizenkaneVII@gmail.com>
2017-10-06 00:37:16 -0600

    Arrays 3

--- Arrays.cs
+++ Arrays.cs
@@ -9,3 +9,7 @@ string grades = "7";
 string grades = "3";
 
 //Instead of dealing with making a bunch of these, we can make an array.
+
+sting[] grades = {"5", "7", "3"};
+
+//An array is a fixed number of elements or a particular type.  All of these can have different values.
