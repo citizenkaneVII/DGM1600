e4f25184bd8af8dc414ca23b8a4b16daf1233aa3
Unknown <citizenkaneVII@gmail.com>
2017-10-07 19:52:19 -0600

    Switch 1

---
+++ Switch Statements.cs
@@ -0,0 +1,3 @@
+/*Switch statements are the alternative to if/then statements.  Similar to how arrays work as an alternate to
+typing out individual variables, switch statements allow us to make a lot of steamlined code that accomplishes
+our needs but cutting out extra fat.*/
