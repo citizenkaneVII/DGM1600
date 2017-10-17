2d4e9ea2d1db575072db6ce4b1dd506f78e09fd7
Unknown <citizenkaneVII@gmail.com>
2017-10-17 00:53:33 -0600

    Switch 9

--- Switch Statements.cs
+++ Switch Statements.cs
@@ -204,3 +204,62 @@ Here are some other examples.*/
             }  
 
     
+    namespace switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opt, num1, num2;
+            float result;
+ 
+        label:
+ 
+            Console.WriteLine("\n\tMenu");
+            Console.WriteLine("\nPress 1 for add");
+            Console.WriteLine("Press 2 for subtraction");
+            Console.WriteLine("Press 3 for multiplication");
+            Console.WriteLine("Press 4 for Division");
+ 
+            Console.Write("\n\nEnter first number:\t");
+            num1 = Convert.ToInt32(Console.ReadLine());
+ 
+            Console.Write("Enter second number:\t");
+            num2 = Convert.ToInt32(Console.ReadLine());
+ 
+            Console.Write("\nEnter your option:\t");
+            opt = Convert.ToInt32(Console.ReadLine());
+ 
+            switch (opt)
+            {
+                case 1:
+                    result = num1 + num2;
+                    Console.WriteLine("\n{0} + {1} = {2}", num1, num2, result);
+                    break;
+ 
+                case 2:
+                    result = num1 - num2;
+                    Console.WriteLine("\n{0} - {1} = {2}", num1, num2, result);
+                    break;
+                case 3:
+                    result = num1 * num2;
+                    Console.WriteLine("\n{0} * {1} = {2}", num1, num2, result);
+                    break;
+                case 4:
+                    result = (float)num1 / num2;
+                    Console.WriteLine("\n{0} / {1} = {2}", num1, num2, result);
+                    break;
+                default:
+                    Console.WriteLine("\nInvalid option. Please try again.");
+                    goto label;
+            }
+            Console.ReadLine();
+        }
+    }   
+}
+
+
+
+
+
+    
