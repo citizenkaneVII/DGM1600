c4790e6da1f5c48e799944350bfa97412170ec97
Unknown <citizenkaneVII@gmail.com>
2017-10-17 00:54:42 -0600

    Switch 10

--- Switch Statements.cs
+++ Switch Statements.cs
@@ -260,6 +260,24 @@ Here are some other examples.*/
 
 
 
-
+string statusCode = "Warn";
+ 
+switch (statusCode)
+{
+    case "Warn":
+        Console.WriteLine("Completed with warnings.");
+        goto case "Complete";
+    case "Err":
+        Console.WriteLine("Completed with errors.");
+        goto case "Complete";
+    case "Complete":
+        Console.WriteLine("Completed.");
+        break;
+    case "Fail":
+        Console.WriteLine("Failed to complete.");
+        break;
+    default:
+        Console.WriteLine("Unknown status code");
+        goto case "Fail";
 
     
