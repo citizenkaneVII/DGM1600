eb62689b56bf4d6c26f700ce3d5060a7c050ffac
Unknown <citizenkaneVII@gmail.com>
2017-10-18 13:52:27 -0600

    For Loops 3

--- For Loops.cs
+++ For Loops.cs
@@ -26,6 +26,10 @@ for (variable initialization; condition; steps)
     //execute this code block as long as condition is satisfied 
 }
 
+/*Each for loop contains three parts.  Remember to declair a for loop by writting "for" first.  In the parenthases
+afterwards, You need to intialize a variable, provide the conditions for the variable to satisfy, and provide the
+steps the loop will take till the condition says they value is false. */
+
 
 /*Loops are bools in the sense that they return a value of true or false.  As long as the method continues to return
 the value of true, the loop will continue to run.  When the value returned is false, the loop ends. */
@@ -34,3 +38,7 @@ for (int i = 0; i < 10; i++)
 {
     Console.WriteLine("Value of i: {0}", i);
 }
+
+/*In the example above, as long as the value of i is less then 10, the code will continue to run, from 0 to 9.
+After 9, it will stop printing i because it will be equel to 10.  The method will then return a value of false
+because i is no longer less then 10, but is equel to 10. */
