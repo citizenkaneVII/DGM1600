5c1f6af80164f43584bfa0c4273c9ebab1e56336
Unknown <citizenkaneVII@gmail.com>
2017-10-18 12:26:31 -0600

    For Loops 1

--- For Loops.cs
+++ For Loops.cs
@@ -1,3 +1,20 @@
 //For Loops
 
+using UnityEngine;
+using System.Collections;
+
+public class ForLoop : MonoBehaviour
+{
+    int numEnemies = 3;
+    
+    
+    void Start ()
+    {
+        for(int i = 0; i < numEnemies; i++)
+        {
+            Debug.Log("Creating enemy number: " + i);
+        }
+    }
+}
+
 /**/
