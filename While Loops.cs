40216551f16d850f1792ac9f9bd79233224bf548
Unknown <citizenkaneVII@gmail.com>
2017-10-21 07:04:43 -0600

    While Loops 2

--- While Loops.cs
+++ While Loops.cs
@@ -18,10 +18,29 @@ public class WhileLoop : MonoBehaviour
     }
 }
 
-/*The idea of the while loop is to run a method while the conditions are still being met to keep the loop going.
-The while loop is interesting in that if an correct exit condition is not provided, it will continue to run as an
-infinite loop.  An infinite loop will use up all the memory and crash Unity, and possibly the computer.  If an
-exit condition is in place, then the method will run till the condition is met. */
+/*The idea of the while loop is to run a method while the conditions are still being met to keep the loop going, in
+otherwards, a true statement.  The while loop is interesting in that if an correct exit condition (a point at which
+the statement condition becomes false) is not provided, it will continue to run as an infinite loop.  An infinite
+loop will use up all the memory and crash Unity, and possibly the computer.  If an exit condition is in place, then 
+the method will run till the condition is met. */
 
 //The syntax of the While loop is:
+
+while(condition)
+{
+   statement(s);
+}
+
+//Here are some examples of while loop code blocks.
+
+ // Continue in while-loop until index is equal to 10.
+        int i = 0;
+        while (i < 10)
+        {
+            Console.Write("While statement ");
+            // Write the index to the screen.
+            Console.WriteLine(i);
+            // Increment the variable.
+            i++;
+        }
 
