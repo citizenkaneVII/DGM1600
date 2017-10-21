ea3e320b37c4882f648dd39556c31c8e5088b7bf
Unknown <citizenkaneVII@gmail.com>
2017-10-21 07:06:23 -0600

    While Loops 3

--- While Loops.cs
+++ While Loops.cs
@@ -44,3 +44,39 @@ while(condition)
             i++;
         }
 
+
+
+static void Main(string[] args)
+      {
+         /* local variable definition */
+         int a = 10;
+
+         /* while loop execution */
+         while (a < 20)
+         {
+            Console.WriteLine("value of a: {0}", a);
+            a++;
+         }
+         Console.ReadLine();
+      }
+
+
+
+static void Main(string[] args)
+        {
+            int num1, res, i;
+ 
+            Console.WriteLine("Enter a number");
+            num1 = Convert.ToInt32(Console.ReadLine());
+ 
+            i = 1; //Initialization
+            //Check whether condition matches or not
+            while (i <= 10)
+            {
+                res = num1 * i;
+                Console.WriteLine("{0} x {1} = {2}", num1, i, res);
+                i++; //Increment by one
+            }
+            Console.ReadLine();
+        }
+
