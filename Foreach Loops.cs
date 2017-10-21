0c0c58991d2fb9802f88814bb20852a8ce9afbdb
Unknown <citizenkaneVII@gmail.com>
2017-10-21 02:22:42 -0600

    Foreach Loops 8

--- Foreach Loops.cs
+++ Foreach Loops.cs
@@ -150,3 +150,18 @@ public static void Main(string[] args)
         }
 
 
+        public static void Main(string[] args)
+        {
+            char[] gender = {'m','f','m','m','m','f','f','m','m','f'};
+            int male = 0, female = 0;
+            foreach (char g in gender)  
+            {
+                if (g == 'm')
+                        male++;
+                else if (g =='f')
+                        female++;
+            }
+            Console.WriteLine("Number of male = {0}", male);
+            Console.WriteLine("Number of female = {0}", female);
+
+
