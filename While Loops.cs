e855492954d053b6aa59266c93da5a623e91847f
Unknown <citizenkaneVII@gmail.com>
2017-10-21 07:07:41 -0600

    While Loops 4

--- While Loops.cs
+++ While Loops.cs
@@ -80,3 +80,14 @@ static void Main(string[] args)
             Console.ReadLine();
         }
 
+
+
+public static void Main(string[] args)
+		{
+			int i=1;
+			while (i<=5)
+			{
+				Console.WriteLine("C# For Loop: Iteration {0}", i);
+				i++;
+			}
+		}
